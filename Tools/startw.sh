4fa1a94810f76d8c05fe4c983af23e1fd7f72142
Zevran <gael.reyrol@hotmail.Fr>
2011-08-14 10:22:01 +0200

    Ajout d'indication dans le fichier startw.sh

--- Tools/startw.sh
+++ Tools/startw.sh
@@ -1,6 +1,8 @@
+#Warning: this file has been edited by Windows, so if you use it directly in a Unix system will not work !
+#Please copy and paste the code into a new file in a Unix system !
 #!/bin/bash
 sleep 1
-cd /home/newmap/core
+cd /home/... #Chemin vers l'exécutable
 while [ 1 == 1 ]
 do
 ./worldserver
